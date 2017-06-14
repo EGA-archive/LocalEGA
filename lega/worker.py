10272b2690721faacafbd416be19337d0137b9fa
Frédéric Haziza <Frederic.Haziza@nbis.se>
2017-06-14 10:38:38 +0200

    Changing the target name in the vault

--- lega/worker.py
+++ lega/worker.py
@@ -106,10 +106,14 @@ def work(data):
                                                target = staging_filepath)
     db.set_encryption(file_id, details, staging_checksum)
     LOG.debug(f'Re-encryption completed')
+
+    #target_name = f"{unencrypted_algo}__{unencrypted_hash}"
+    target_name = f"{file_id:0>20}" # filling with zeros, and 20 characters wide
+
     reply = {
         'file_id' : file_id,
         'filepath': str(staging_filepath),
-        'target_name': f"{unencrypted_algo}__{unencrypted_hash}",
+        'target_name': target_name,
         'user_id': user_id,
     }
     LOG.debug(f"Reply message: {reply!r}")
