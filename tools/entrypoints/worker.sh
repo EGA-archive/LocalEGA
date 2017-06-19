149a18d62d07f9ed76dc92170a4818e7325284ed
Frédéric Haziza <Frederic.Haziza@nbis.se>
2017-06-19 19:08:42 +0200

    Fixing the GPG agent

--- tools/entrypoints/worker.sh
+++ tools/entrypoints/worker.sh
@@ -8,18 +8,17 @@ pkill gpg-agent || true
 
 HEXPWD=$(python -c "import binascii; print(binascii.hexlify(b'${PASSPHRASE}'))")
 
-TMP_GPG=/tmp/gnupg/
+AGENT_ENV=/tmp/gpg-agent.env
 
 # GPG agent and homedir
-rm -f $TMP_GPG/agent.env
-gpg-agent --daemon --homedir $GNUPGHOME --write-env-file $TMP_GPG/agent.env
-echo "export GPG_AGENT_INFO" >> $TMP_GPG/agent.env
-echo "export GNUPGHOME=$GNUPGHOME" >> $TMP_GPG/agent.env
-source $TMP_GPG/agent.env
+rm -f $AGENT_ENV
+gpg-agent --daemon --homedir $GNUPGHOME --write-env-file $AGENT_ENV
 
 KEYGRIP=$(gpg --homedir $GNUPGHOME --fingerprint --fingerprint ega@nbis.se |\
 	      grep fingerprint | tail -1 | cut -d= -f2 | sed -e 's/ //g')
-gpg-preset-passphrase --preset -P $PASSPHRASE $KEYGRIP
+
+source $AGENT_ENV
+/usr/libexec/gpg-preset-passphrase --preset -P $PASSPHRASE $KEYGRIP
 
 sleep 6
 exec ega-worker
