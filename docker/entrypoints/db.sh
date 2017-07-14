1429b90bf829ee25a9f53e44ead99433b4cef287
Frédéric Haziza <Frederic.Haziza@nbis.se>
2017-07-14 23:59:35 +0200

    Using PgAdmin4 in a virtualenv

--- docker/entrypoints/db.sh
+++ docker/entrypoints/db.sh
@@ -2,6 +2,9 @@
 
 set -e
 
-python /usr/local/lib/python2.7/dist-packages/pgadmin4/pgAdmin4.py &
+( # new shell
+    workon pgadmin4
+    python /usr/local/lib/python2.7/dist-packages/pgadmin4/pgAdmin4.py
+) &
 
 exec /usr/local/bin/docker-entrypoint.sh postgres
--- docker/images/db/config_local.py
+++ docker/images/db/config_local.py
@@ -6,21 +6,26 @@ APP_NAME = 'EGA - pgAdmin4'
 
 # Debug mode
 DEBUG = True
-SERVER_MODE = True
+SERVER_MODE = False
 CONSOLE_LOG_LEVEL = DEBUG
 FILE_LOG_LEVEL = DEBUG
 
-# Folders
+# Misc
 LOG_FILE = '/var/log/pgadmin4/pgadmin4.log'
 SQLITE_PATH = '/var/lib/pgadmin4/pgadmin4.db'
 SESSION_DB_PATH = '/var/lib/pgadmin4/sessions'
 STORAGE_DIR = '/var/lib/pgadmin4/storage'
 
 # Binding
-DEFAULT_SERVER = 'localhost'
+DEFAULT_SERVER = '0.0.0.0'
 DEFAULT_SERVER_PORT = 5050
 
 # Extra
 MODULE_BLACKLIST.remove('test')
 UPGRADE_CHECK_ENABLED = False
 DESKTOP_USER = 'ega@nbis.se'
+
+
+# import os
+# os.environ['PGADMIN_SETUP_USER'] = 'ega@nbis.se'
+# os.environ['PGADMIN_SETUP_PASSWORD'] = 'hello2you'
