994e8b7bbece124a93fe8fad17232c9e31035fe4
Frédéric Haziza <Frederic.Haziza@nbis.se>
2017-08-11 14:10:39 +0200

    Clean up

--- terraform/images/centos7/db.sh
+++ terraform/images/centos7/db.sh
@@ -5,8 +5,6 @@ yum -y install https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x
 yum -y install postgresql96-server postgresql96-contrib
 
 /usr/pgsql-9.6/bin/postgresql96-setup initdb
-systemctl enable postgresql-9.6.service
-
 
 mv /var/lib/pgsql/9.6/data/postgresql.conf /var/lib/pgsql/9.6/data/postgresql.conf.old
 grep -v '^$\|^\s*\#' /var/lib/pgsql/9.6/data/postgresql.conf.old > /var/lib/pgsql/9.6/data/postgresql.conf
@@ -18,6 +16,7 @@ sed -i -e "s/local\(.*\)peer/local\1trust/" /var/lib/pgsql/9.6/data/pg_hba.conf
 sed -i -e "s;host.*1/128.*ident;host all all all md5;" /var/lib/pgsql/9.6/data/pg_hba.conf
 
 systemctl start postgresql-9.6.service
+systemctl enable postgresql-9.6.service
 
 
 
