c8bee65018a0c995642e4ebd8480b7c0e22d1fc5
Frédéric Haziza <Frederic.Haziza@nbis.se>
2017-08-22 17:57:25 +0200

    Adding some nss code

---
+++ src/auth/nss/interface.c
@@ -0,0 +1,95 @@
+#include "nss-ega.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+/*
+ * passwd functions
+ */
+enum nss_status
+_nss_ega_setpwent (void)
+{
+  enum nss_status retval = NSS_STATUS_UNAVAIL;
+
+  pthread_mutex_lock(&lock);
+
+  if(backend_open()) {
+    retval = NSS_STATUS_SUCCESS;
+  }
+
+  pthread_mutex_unlock(&lock);
+
+  return retval;
+}
+
+enum nss_status
+_nss_ega_endpwent(void)
+{
+  pthread_mutex_lock(&lock);
+  backend_close();
+  pthread_mutex_unlock(&lock);
+  
+  return NSS_STATUS_SUCCESS;
+}
+
+enum nss_status
+_nss_ega_getpwent_r(struct passwd *result,
+		    char *buffer, size_t buflen,
+		    int *errnop)
+{
+  enum nss_status retval = NSS_STATUS_UNAVAIL;
+  int lerrno = 0;
+
+  pthread_mutex_lock(&lock);
+
+  if(backend_open()) {
+    retval = backend_getpwent(result, buffer, buflen, &lerrno);
+  }
+  *errnop = lerrno;
+  pthread_mutex_unlock(&lock);
+  
+  return retval;
+}
+
+enum nss_status
+_nss_ega_getpwnam_r(const char *pwnam,
+		    struct passwd *result,
+		    char *buffer, size_t buflen,
+		    int *errnop)
+{
+  enum nss_status retval = NSS_STATUS_UNAVAIL;
+  int lerrno = 0;
+
+  pthread_mutex_lock(&lock);
+  if(backend_open()) {
+    retval = backend_getpwnam(pwnam, result, buffer, buflen, &lerrno);
+  }
+  backend_close();
+  *errnop = lerrno;
+  pthread_mutex_unlock(&lock);
+
+  return retval;
+}
+
+enum nss_status
+_nss_ega_getpwuid_r(uid_t uid,
+		    struct passwd *result,
+		    char *buffer, size_t buflen,
+		    int *errnop)
+{
+  enum nss_status retval = NSS_STATUS_UNAVAIL;
+  int lerrno = 0;
+
+  pthread_mutex_lock(&lock);
+  if(backend_open()) {
+    retval = backend_getpwuid(uid, result, buffer, buflen, &lerrno);
+  }
+  *errnop = lerrno;
+  backend_close();
+  pthread_mutex_unlock(&lock);
+
+  return retval;
+}
+
---
+++ src/auth/nss/nss-ega.h
@@ -0,0 +1,47 @@
+#ifndef __NSS_EGA_H_INCLUDED__
+#define __NSS_EGA_H_INCLUDED__
+
+/* #include <unistd.h> */
+#include <nss.h>
+#include <pwd.h>
+#include <sys/types.h>
+
+#define CFGFILE "/usr/local/etc/nss-ega.conf"
+
+int readconfig(char type, char* configfile);
+void cleanup(void);
+char *getcfg(const char *key);
+
+int backend_isopen();
+int backend_open();
+void backend_close();
+
+enum nss_status backend_getpwuid(uid_t uid,
+				 struct passwd *result,
+				 char *buffer,
+				 size_t buflen,
+				 int *errnop);
+enum nss_status backend_getpwnam(const char *name,
+				 struct passwd *result,
+				 char *buffer,
+				 size_t buflen,
+				 int *errnop);
+
+#ifdef DEBUG
+#include <syslog.h>
+#define DBGLOG(x...)  do {                                          \
+                          openlog("NSS_pgsql", LOG_PID, LOG_AUTH);  \
+                          syslog(LOG_DEBUG, ##x);                   \
+                          closelog();                               \
+                      } while(0);
+#define SYSLOG(x...)  do {                                          \
+                          openlog("NSS_pgsql", LOG_PID, LOG_AUTH);  \
+                          syslog(LOG_INFO, ##x);                    \
+                          closelog();                               \
+                      } while(0);
+#else
+#define DBGLOG(x...)
+#define SYSLOG(x...)
+#endif /* !DEBUG */
+
+#endif /* !__NSS_PGSQL_H_INCLUDED__ */
