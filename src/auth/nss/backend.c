8e2359000c0a0d2302e20127f238d020d8661fd3
Frédéric Haziza <Frederic.Haziza@nbis.se>
2017-08-23 13:25:22 +0200

    Debugging NSS code

--- src/auth/nss/backend.c
+++ src/auth/nss/backend.c
@@ -6,6 +6,14 @@
 #include <stdio.h>
 #include <errno.h>
 
+/* define passwd column names */
+#define PASSWD_NAME   0
+#define PASSWD_PASSWD 1
+#define PASSWD_UID    2
+#define PASSWD_GID    3
+#define PASSWD_GECOS  4
+#define PASSWD_DIR    5
+#define PASSWD_SHELL  6
 
 static PGconn *_conn = NULL;
 static int _isopen = 0;
@@ -13,14 +21,15 @@ static int _isopen = 0;
 /*
  * read configuration and connect to database
  */
-int backend_isopen()
+int backend_isopen(void)
 {
   return (_isopen > 0);
 }
 
-int backend_open()
+int backend_open(void)
 {
   int config;
+  char* connection;
 
   if(!_isopen) {
     config = readconfig(CFGFILE);
@@ -28,13 +37,15 @@ int backend_open()
       if (_conn != NULL) {
 	PQfinish(_conn);
       }
-      _conn = PQconnectdb(getcfg("connection"));
+      connection = getcfg("connection");
+      DBGLOG("Connection to: %s",connection);
+      _conn = PQconnectdb(connection);
     }
 	  
     if(PQstatus(_conn) == CONNECTION_OK) {
       ++_isopen;
     } else {
-      print_msg("\nCould not connect to database\n");
+      SYSLOG("Could not connect to database");
     }
   }
   return (_isopen > 0);
@@ -43,7 +54,7 @@ int backend_open()
 /*
  * close connection to database and clean up configuration
  */
-void backend_close()
+void backend_close(void)
 {
   --_isopen;
   if(!_isopen) {
@@ -64,38 +75,24 @@ copy_attr_colnum(PGresult *res, int attrib_number, char **valptr,
                  char **buffer, size_t *buflen, int *errnop, int row)
 {
 
-	const char *sptr;
-	size_t slen;
+  const char *sptr;
+  size_t slen;
 
-	sptr = PQgetvalue(res, row, attrib_number);
-	return copy_attr_string(sptr, valptr, buffer, buflen, errnop);
-}
-
-/*
-  With apologies to nss_ldap...
-  Assign a single value to *valptr from the specified row in the result
-*/
-enum nss_status
-copy_attr_string(char *sptr, char **valptr,
-                 char **buffer, size_t *buflen, int *errnop)
-{
-
-	size_t slen;
-
-	slen = strlen(sptr);
-	if(*buflen < slen + 1) {
-		*errnop = ERANGE;
-		return NSS_STATUS_TRYAGAIN;
-	}
-	strncpy(*buffer, sptr, slen);
-	(*buffer)[slen] = '\0';
-		
-	*valptr = *buffer;
-
-	*buffer += slen + 1;
-	*buflen -= slen + 1;
-
-	return NSS_STATUS_SUCCESS;
+  sptr = PQgetvalue(res, row, attrib_number);
+  slen = strlen(sptr);
+  if(*buflen < slen+1) {
+    *errnop = ERANGE;
+    return NSS_STATUS_TRYAGAIN;
+  }
+  strncpy(*buffer, sptr, slen);
+  (*buffer)[slen] = '\0';
+  
+  *valptr = *buffer;
+  
+  *buffer += slen + 1;
+  *buflen -= slen + 1;
+  
+  return NSS_STATUS_SUCCESS;
 }
 
 /*
@@ -105,47 +102,44 @@ enum nss_status res2pwd(PGresult *res, struct passwd *result,
                         char *buffer, size_t buflen,
 			int *errnop)
 {
-	enum nss_status status = NSS_STATUS_NOTFOUND;
-	char* tmp;
-	char* homes;
-	if(!PQntuples(res)) {
-		goto BAIL_OUT;
-	}
+  const char *sptr;
+  enum nss_status status = NSS_STATUS_NOTFOUND;
+  if(!PQntuples(res)) {
+    goto BAIL_OUT;
+  }
 
-	// Carefully copy attributes into buffer.  Return NSS_STATUS_TRYAGAIN if not enough room.
-	// Must return passwd_name, passwd_passwd
-	status = copy_attr_colnum(res, 0, &result->pw_name, &buffer, &buflen, errnop, 0);
-	if(status != NSS_STATUS_SUCCESS) goto BAIL_OUT;
+  status = copy_attr_colnum(res, PASSWD_NAME, &result->pw_name, &buffer, &buflen, errnop, 0);
+  if(status != NSS_STATUS_SUCCESS) goto BAIL_OUT;
 
-	status = copy_attr_colnum(res, 1, &result->pw_passwd, &buffer, &buflen, errnop, 0);
-	if(status != NSS_STATUS_SUCCESS) goto BAIL_OUT;
+  status = copy_attr_colnum(res, PASSWD_PASSWD, &result->pw_passwd, &buffer, &buflen, errnop, 0);
+  if(status != NSS_STATUS_SUCCESS) goto BAIL_OUT;
 
-	// almost Fixed params
-	homes = getcfg("ega_homes");
-	tmp = malloc(strlen(homes) + strlen(result->pw_name) + 1);
-	strcpy(tmp, homes);
-	strcat(tmp, result->pw_name);
-	result->pw_dir = tmp;
+  status = copy_attr_colnum(res, PASSWD_GECOS, &result->pw_gecos, &buffer, &buflen, errnop, 0);
+  if(status != NSS_STATUS_SUCCESS) goto BAIL_OUT;
 
-	result->pw_gecos = getcfg("ega_gecos");
+  status = copy_attr_colnum(res, PASSWD_DIR, &result->pw_dir, &buffer, &buflen, errnop, 0);
+  if(status != NSS_STATUS_SUCCESS) goto BAIL_OUT;
 
-	result->pw_uid = (uid_t) strtoul(getcfg("ega_uid"), (char**)NULL, 10);
+  status = copy_attr_colnum(res, PASSWD_SHELL, &result->pw_shell, &buffer, &buflen, errnop, 0);
+  if(status != NSS_STATUS_SUCCESS) goto BAIL_OUT;
 
-	result->pw_gid = (gid_t) strtoul(getcfg("ega_gid"), (char**)NULL, 10);
+  // Can be less careful with uid/gid
+  result->pw_uid = (uid_t) strtoul(PQgetvalue(res, 0, PASSWD_UID), (char**)NULL, 10);
+  result->pw_gid = (gid_t) strtoul(PQgetvalue(res, 0, PASSWD_GID), (char**)NULL, 10);
 
-	result->pw_shell = getcfg("ega_shell");
-
-	DBGLOG("Converted a res to a pwd:");
-	DBGLOG("UID: %d", result->pw_uid);
-	DBGLOG("GID: %d", result->pw_gid);
-	DBGLOG("Name: %s", result->pw_name);
-	DBGLOG("Password: %s", result->pw_passwd);
-	DBGLOG("Gecos: %s", result->pw_gecos);
-	DBGLOG("Dir: %s", result->pw_dir);
-	DBGLOG("Shell: %s", result->pw_shell);
+#ifdef DEBUG
+  DBGLOG("Converted a res to a pwd:");
+  DBGLOG("UID: %d", result->pw_uid);
+  DBGLOG("GID: %d", result->pw_gid);
+  DBGLOG("Name: %s", result->pw_name);
+  DBGLOG("Password: %s", result->pw_passwd);
+  DBGLOG("Gecos: %s", result->pw_gecos);
+  DBGLOG("Dir: %s", result->pw_dir);
+  DBGLOG("Shell: %s", result->pw_shell);
+#endif
 
 BAIL_OUT:
-	return status;
+  return status;
 }
 
 /*
@@ -153,54 +147,52 @@ BAIL_OUT:
  */
 enum nss_status backend_getpwnam(const char *name,
 				 struct passwd *result,
-                                 char *buffer,
-				 size_t buflen,
+                                 char *buffer, size_t buflen,
 				 int *errnop)
 {
-	const char *params[1];
-	PGresult *res;
-	enum nss_status status = NSS_STATUS_NOTFOUND;
+  const char *params[1];
+  PGresult *res;
+  enum nss_status status = NSS_STATUS_NOTFOUND;
 
-	params[0] = name;
+  params[0] = name;
 
-	res = PQexecParams(_conn, getcfg("ega_query"), 1, NULL, params, NULL, NULL, 0);
-	if(PQresultStatus(res) == PGRES_TUPLES_OK) {
-		// Fill result structure with data from the database
-		status = res2pwd(res, result, buffer, buflen, errnop);
-	}
+  res = PQexecParams(_conn, getcfg("getpwnam"), 1, NULL, params, NULL, NULL, 0);
+  if(PQresultStatus(res) == PGRES_TUPLES_OK) {
+    // Fill result structure with data from the database
+    status = res2pwd(res, result, buffer, buflen, errnop);
+  }
 
-	PQclear(res);
-	return status;
+  PQclear(res);
+  return status;
 }
 
 /*
- * backend for getpwuid()
+ * get a passwd entry from cursor
  */
-enum nss_status backend_getpwuid(uid_t uid,
-				 struct passwd *result,
-				 char *buffer,
-				 size_t buflen,
-				 int *errnop)
+enum nss_status
+backend_getpwent(struct passwd *result,
+		 char *buffer, size_t buflen,
+		 int *errnop)
 {
-	char *params[1];
-	int n;
-	PGresult *res;
-	enum nss_status status = NSS_STATUS_NOTFOUND;
-   
-   	params[0] = malloc(12);
-	n = snprintf(params[0], 12, "%d", uid);
-	if (n == -1 || n > 12) {
-		status = NSS_STATUS_UNAVAIL;
-		*errnop = EAGAIN;
-	} else {
-		res = PQexecParams(_conn, getcfg("getuser"), 1, NULL, (const char**)params, NULL, NULL, 0);
+  PGresult *res;
+  enum nss_status status = NSS_STATUS_NOTFOUND;
 
-		if(PQresultStatus(res) == PGRES_TUPLES_OK) {
-			// Fill result structure with data from the database
-			status = res2pwd(res, result, buffer, buflen, errnop);
-		}
-		PQclear(res);
-    }
-	free(params[0]);
-	return status;
+  /* res = PQexecParams(_conn, getcfg("getpwent"), 1, NULL, params, NULL, NULL, 0); */
+  /* if(PQresultStatus(res) == PGRES_TUPLES_OK) { */
+  /*   status = res2pwd(res, result, buffer, buflen, errnop); */
+  /* } */
+  /* PQclear(res); */
+  return status;
+}   
+
+/*
+ * backend for getpwuid(): Unused
+ */
+enum nss_status
+backend_getpwuid(uid_t uid,
+		 struct passwd *result,
+		 char *buffer, size_t buflen,
+		 int *errnop)
+{
+  return NSS_STATUS_NOTFOUND;
 }
