2243d4905f26c4e2550cdf210806971d9a517f5a
Frédéric Haziza <Frederic.Haziza@nbis.se>
2017-08-24 16:15:40 +0200

    Cleaning up

--- src/auth/nss/nss-ega.h
+++ src/auth/nss/nss-ega.h
@@ -1,10 +1,15 @@
 #ifndef __NSS_EGA_H_INCLUDED__
 #define __NSS_EGA_H_INCLUDED__
 
+#include <stdio.h>
+#include <stdlib.h>
 /* #include <unistd.h> */
 #include <nss.h>
 #include <pwd.h>
 #include <sys/types.h>
+#ifdef DEBUG
+#include <syslog.h>
+#endif
 
 #define CFGFILE "/usr/local/etc/nss-ega.conf"
 
@@ -19,38 +24,30 @@ void backend_close(void);
 enum nss_status backend_getpwuid(uid_t uid,
 				 struct passwd *result,
 				 char *buffer, size_t buflen,
-				 int *errnop)
+				 int *errnop);
   
 enum nss_status backend_getpwnam(const char *name,
 				 struct passwd *result,
-				 char *buffer,
-				 size_t buflen,
+				 char *buffer, size_t buflen,
 				 int *errnop);
 
 enum nss_status backend_getpwent(struct passwd *result,
 				 char *buffer, size_t buflen,
-				 int *errnop)
+				 int *errnop);
 
 
 #ifdef DEBUG
-#ifdef DEBUG_SYSLOG
-#include <syslog.h>
-#endif /* !DEBUG_SYSLOG */
 #define DBGLOG(x...)  do {                                          \
                           fprintf(stderr, ##x);                     \
-#ifdef DEBUG_SYSLOG
                           openlog("NSS_ega", LOG_PID, LOG_USER);    \
                           syslog(LOG_DEBUG, ##x);                   \
                           closelog();                               \
-#endif /* !DEBUG_SYSLOG */
                       } while(0);
 #define SYSLOG(x...)  do {                                          \
                           fprintf(stderr, ##x);                     \
-#ifdef DEBUG_SYSLOG
                           openlog("NSS_ega", LOG_PID, LOG_USER);    \
                           syslog(LOG_INFO, ##x);                    \
                           closelog();                               \
-#endif /* !DEBUG_SYSLOG */
                       } while(0);
 #else
 #define DBGLOG(x...)
