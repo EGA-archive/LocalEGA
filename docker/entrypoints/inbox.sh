623a54dc8945635866016f28aed0efb13e3db425
Frédéric Haziza <Frederic.Haziza@nbis.se>
2017-09-08 21:30:46 +0200

    Adjusting so that the inbox uses the NSS and PAM modules.
    Note that we now create volumes instead of sharing one from the host
    machine. The previous sharing raised the issue of the setgid not
    working, and that made the sftp login fail (because of chroot and bad
    permissions).
    
    There are 3 volumes:
    - inbox (mounted on the inbox and each worker)
    - staging (mounted on each worker and the vault)
    - vault (mounted on the vault only)

--- docker/entrypoints/inbox.sh
+++ docker/entrypoints/inbox.sh
@@ -2,12 +2,32 @@
 
 set -e
 
+chown root:ega /ega/inbox
+chmod 750 /ega/inbox
+chmod g+s /ega/inbox # setgid bit
+
 pushd /root/ega/auth/nss
 make
 make install
 popd
 
 ldconfig -v
+
+cat > /etc/pam_pgsql.conf <<EOF
+database=lega
+table=users
+host=ega-db
+port=5432
+user=postgres
+password=${POSTGRES_PASSWORD}
+debug=1
+pw_type=crypt_sha512
+timeout=15
+sslmode=disable
+pwd_column=password_hash
+table=users
+user_column=elixir_id
+EOF
 
 cat > /usr/local/etc/nss-ega.conf <<EOF
 connection = host=ega-db port=5432 dbname=lega user=postgres password=${POSTGRES_PASSWORD} connect_timeout=1 sslmode=disable
@@ -27,14 +47,14 @@ getspent = SELECT elixir_id,password_hash,'','','','','','','' FROM users LIMIT 
 EOF
 
 
+pip install -e /root/ega
+
 echo "Waiting for Message Broker"
 until nc -4 --send-only ega-mq 5672 </dev/null &>/dev/null; do sleep 1; done
 echo "Waiting for database"
 until nc -4 --send-only ega-db 5432 </dev/null &>/dev/null; do sleep 1; done
 
-
 echo "Starting the inbox listener"
-pip install -e /root/ega
 ega-inbox &
 
 echo "Starting the SFTP server"
