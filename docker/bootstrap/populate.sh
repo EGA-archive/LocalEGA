a6181dbb7267b64c4748026d53880639e0844d84
Frédéric Haziza <Frederic.Haziza@nbis.se>
2017-10-06 09:48:16 +0200

    Parameterizing the entrypoints folder

--- docker/bootstrap/populate.sh
+++ docker/bootstrap/populate.sh
@@ -7,12 +7,14 @@ HERE=$PWD/${SCRIPT#./}
 VERBOSE=yes
 FORCE=no
 PRIVATE=private
+ENTRYPOINTS=$HERE/../entrypoints
 
 function usage {
     echo "Usage: $0 [options]"
     echo -e "\nOptions are:"
-    echo -e "\t--private_dir <name>         \tName of the main folder for private data"
-    echo -e "\t--force, -f                  \tForce the re-creation of the subfolders"
+    echo -e "\t--private_dir <path>         \tPath location of private data folder"
+    echo -e "\t--entrypoints <path>         \tPath Location of the entrypoints folder"
+    echo -e "\t--force, -f                  \tDon't backup .env and .env.d if they exist"
     echo -e "\t--quiet, -q                  \tRemoves the verbose output (and uses -f)"
     echo -e "\t--help, -h                   \tOutputs this message and exits"
     echo -e "\t-- ...                       \tAny other options appearing after the -- will be ignored"
@@ -25,6 +27,7 @@ while [[ $# -gt 0 ]]; do
         --quiet|-q) VERBOSE=no;;
         --help|-h) usage; exit 0;;
         --force|-f) FORCE=yes;;
+        --entrypoints) ENTRYPOINTS=$2; shift;;
         --private_dir) PRIVATE=$2; shift;;
         --) shift; break;;
         *) echo "$0: error - unrecognized option $1" 1>&2; usage; exit 1;;
@@ -32,16 +35,25 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
+function echomsg {
+    [[ $VERBOSE == 'yes' ]] && echo $@
+}
+
 case $PRIVATE in
     /*)  ABS_PRIVATE=$PRIVATE;;
     ./*|../*) ABS_PRIVATE=$PWD/$PRIVATE;;
     *) ABS_PRIVATE=$HERE/$PRIVATE;;
 esac
 
-[[ -d $ABS_PRIVATE ]] || { echo "Private data folder $ABS_PRIVATE not found. Exiting"; exit 1; }
+[[ -d $ABS_PRIVATE ]] || { echomsg "Private data folder $ABS_PRIVATE not found. Exiting" 1>&2; exit 1; }
 
-echo "Copying the docker-compose environment"
-DEST=$HERE/..
+case $ENTRYPOINTS in
+    /*)  ABS_ENTRYPOINTS=$ENTRYPOINTS;;
+    ./*|../*) ABS_ENTRYPOINTS=$PWD/$ENTRYPOINTS;;
+    *) ABS_ENTRYPOINTS=$HERE/$ENTRYPOINTS;;
+esac
+
+[[ -d $ABS_ENTRYPOINTS ]] || { echomsg "Entrypoints folder $ABS_ENTRYPOINTS not found. Exiting" 1>&2; exit 1; }
 
 function backup {
     local target=$1
@@ -51,16 +63,16 @@ function backup {
 }
 
 [[ $FORCE == 'yes' ]] || {
-    echo "Backup first"
+    echomsg "Backing up"
     backup $HERE/../.env
     backup $HERE/../.env.d
 }
 
 # Populate env-settings for docker compose
-echo "Creating the docker-compose configuration files"
 cat > $HERE/../.env <<EOF
 COMPOSE_PROJECT_NAME=ega
 COMPOSE_FILE=ega.yml
+ENTRYPOINTS=${ABS_ENTRYPOINTS}
 CONF=$ABS_PRIVATE/ega.conf
 KEYS=$ABS_PRIVATE/keys.conf
 SSL_CERT=$ABS_PRIVATE/certs/ssl.cert
@@ -72,5 +84,6 @@ CEGA_USERS=$ABS_PRIVATE/cega/users
 CEGA_MQ_DEFS=$ABS_PRIVATE/cega/mq/defs.json
 EOF
 
-cp -rf $ABS_PRIVATE/.env.d $DEST/.env.d
+cp -rf $ABS_PRIVATE/.env.d $HERE/../.env.d
 
+echomsg "docker-compose configuration files populated"
