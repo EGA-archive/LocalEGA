63798a16a969b054f6ea04544c7d07575453d67f
Juha Törnroos <juha.tornroos@csc.fi>
2017-11-28 17:09:30 +0100

    Remove src/auth because it lives now in own repo

--- src/auth/cega.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LEGA_CENTRAL_H_INCLUDED__
-#define __LEGA_CENTRAL_H_INCLUDED__
-
-#include <stdbool.h>
-
-bool fetch_from_cega(const char *username, char **buffer, size_t *buflen, int *errnop);
-
-#endif /* !__LEGA_CENTRAL_H_INCLUDED__ */
