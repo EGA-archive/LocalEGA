feb4366aa6eec6fedcb1b3f4dd8a099ab434f2c4
Frédéric Haziza <Frederic.Haziza@nbis.se>
2017-11-28 22:23:23 +0100

    Remove hard-coded gpg email for presetting the passphrase

--- deployments/docker/images/keys/entrypoint.sh
+++ deployments/docker/images/keys/entrypoint.sh
@@ -17,12 +17,12 @@ ${GPG_AGENT} --daemon
 # This should create /run/ega/S.gpg-agent{,.extra,.ssh}
 
 #while gpg-connect-agent /bye; do sleep 2; done
-KEYGRIP=$(${GPG} -k --with-keygrip ega@nbis.se | awk '/Keygrip/{print $3;exit;}')
-${GPG_PRESET} --preset -P $GPG_PASSPHRASE $KEYGRIP
+KEYGRIP=$(${GPG} -k --with-keygrip ${GPG_EMAIL} | awk '/Keygrip/{print $3;exit;}')
+${GPG_PRESET} --preset -P ${GPG_PASSPHRASE} ${KEYGRIP}
 unset GPG_PASSPHRASE
 
 echo "Starting the gpg-agent proxy"
-ega-socket-proxy "0.0.0.0:$KEYSERVER_PORT" /root/.gnupg/S.gpg-agent --certfile /etc/ega/ssl.cert --keyfile /etc/ega/ssl.key &
+ega-socket-proxy "0.0.0.0:${KEYSERVER_PORT}" /root/.gnupg/S.gpg-agent --certfile /etc/ega/ssl.cert --keyfile /etc/ega/ssl.key &
 
 echo "Starting the key management server"
 exec ega-keyserver --keys /etc/ega/keys.ini
