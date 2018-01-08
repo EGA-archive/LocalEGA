adc65a2b820de2641745ae9d5ad3d4bb494a2633
Dmytro Titov <dmytro.titov@gmail.com>
2018-01-08 10:41:21 +0100

    Refactor tests.

--- tests/src/test/java/se/nbis/lega/cucumber/Utils.java
+++ tests/src/test/java/se/nbis/lega/cucumber/Utils.java
@@ -1,5 +1,6 @@
 package se.nbis.lega.cucumber;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.CreateContainerResponse;
 import com.github.dockerjava.api.model.Bind;
@@ -8,25 +9,25 @@ import com.github.dockerjava.api.model.Volume;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.command.ExecStartResultCallback;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import se.nbis.lega.cucumber.publisher.Message;
 import se.nbis.lega.cucumber.publisher.Checksum;
+import se.nbis.lega.cucumber.publisher.Message;
 
+import javax.ws.rs.core.MediaType;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
@@ -263,40 +264,44 @@ public class Utils {
      * Sends a JSON message to a RabbitMQ instance.
      *
      * @param connection The address of the broker.
-     * @return if the message was sent.
-     * @throws IOException In case of broken connection.
+     * @param user       Username.
+     * @param filename   Filename.
+     * @param enc        Encrypted file hash (MD5).
+     * @param unenc      Unencrypted file hash (MD5).
+     * @throws Exception In case of broken connection.
      */
-    public void publishCega(String connection, String user, String filename, String enc, String unenc) throws Exception {
-	
-	Message message = new Message();
-	message.setElixirId(user);
+    public void publishCEGA(String connection, String user, String filename, String enc, String unenc) throws Exception {
+        Message message = new Message();
+        message.setElixirId(user);
         message.setFilename(filename);
 
-	Checksum unencrypted = new Checksum();
-	unencrypted.setAlgorithm("md5");
-	unencrypted.setHash(unenc);
-	message.setUnencryptedIntegrity(unencrypted);
+        Checksum unencrypted = new Checksum();
+        unencrypted.setAlgorithm("md5");
+        unencrypted.setHash(unenc);
+        message.setUnencryptedIntegrity(unencrypted);
 
-	Checksum encrypted = new Checksum();
-	encrypted.setAlgorithm("md5");
-	encrypted.setHash(enc);
-	message.setEncryptedIntegrity(encrypted);
+        Checksum encrypted = new Checksum();
+        encrypted.setAlgorithm("md5");
+        encrypted.setHash(enc);
+        message.setEncryptedIntegrity(encrypted);
 
-	ConnectionFactory factory = new ConnectionFactory();
-	factory.setUri(connection);
-	
-	Connection connectionFactory = factory.newConnection();
-	Channel channel = connectionFactory.createChannel();
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUri(connection);
 
-	ObjectMapper objectMapper = new ObjectMapper();
-	AMQP.BasicProperties properties = new AMQP.BasicProperties().builder().deliveryMode(2)
-	    .contentType("application/json").contentEncoding("UTF-8").build();
-	
-	channel.basicPublish("localega.v1", "files", properties,
-			     objectMapper.writeValueAsBytes(message));
+        Connection connectionFactory = factory.newConnection();
+        Channel channel = connectionFactory.createChannel();
 
-	channel.close();
-	connectionFactory.close();
+        ObjectMapper objectMapper = new ObjectMapper();
+        AMQP.BasicProperties properties = new AMQP.BasicProperties().builder().
+                deliveryMode(2).
+                contentType(MediaType.APPLICATION_JSON_TYPE.getType()).
+                contentEncoding(StandardCharsets.UTF_8.displayName()).
+                build();
+
+        channel.basicPublish("localega.v1", "files", properties, objectMapper.writeValueAsBytes(message));
+
+        channel.close();
+        connectionFactory.close();
     }
 
 }
