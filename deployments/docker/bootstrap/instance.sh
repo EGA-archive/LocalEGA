603162389231ad64cfbf141a5bf0c8c593beb56e
Dmytro Titov <dmytro.titov@gmail.com>
2018-01-10 13:55:26 +0100

    Assign wider permissions to a GPG folder.

--- deployments/docker/bootstrap/instance.sh
+++ deployments/docker/bootstrap/instance.sh
@@ -45,7 +45,7 @@ EOF
 
 ${GPG} --homedir ${PRIVATE}/${INSTANCE}/gpg --batch --generate-key ${PRIVATE}/${INSTANCE}/gen_key
 ${GPG} --homedir ${PRIVATE}/${INSTANCE}/gpg --armor --export -a "${GPG_NAME}" > ${PRIVATE}/${INSTANCE}/gpg/public.key
-chmod 744 ${PRIVATE}/${INSTANCE}/gpg
+chmod 755 ${PRIVATE}/${INSTANCE}/gpg
 chmod 744 ${PRIVATE}/${INSTANCE}/gpg/public.key
 rm -f ${PRIVATE}/${INSTANCE}/gen_key
 ${GPG_CONF} --kill gpg-agent
