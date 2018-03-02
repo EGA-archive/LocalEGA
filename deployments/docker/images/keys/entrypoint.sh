9f1994c97dd64736b0ae48b81d9ed731f1b095e9
Frédéric Haziza <Frederic.Haziza@nbis.se>
2018-03-02 18:17:34 +0100

    Removed the unnecessary entrypoint script for the keyserver.

--- deployments/docker/images/keys/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# KEYSERVER_PORT env must be defined
-[[ -z "$KEYSERVER_PORT" ]] && echo 'Environment KEYSERVER_PORT is empty' 1>&2 && exit 1
-
-echo "Starting the key management server"
-exec ega-keyserver --keys /etc/ega/keys.ini
