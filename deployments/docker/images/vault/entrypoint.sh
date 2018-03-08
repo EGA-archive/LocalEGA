fd0d6bf7ea2a184158c3bafbe4885d8fd561eb6c
Frédéric Haziza <Frederic.Haziza@nbis.se>
2018-03-08 23:50:30 +0100

    Trying 2 big images (base and inbox) containing everything.
    They are uploaded to docker.io already and we don't build the images.

--- deployments/docker/images/vault/entrypoint.sh
+++ deployments/docker/images/vault/entrypoint.sh
@@ -7,9 +7,9 @@ set -e
 [[ -z "$CEGA_INSTANCE" ]] && echo 'Environment CEGA_INSTANCE is empty' 1>&2 && exit 1
 
 echo "Waiting for Central Message Broker"
-until nc -4 -z ${CEGA_INSTANCE} 5672 </dev/null &>/dev/null; do sleep 1; done
+until nc -4 --send-only ${CEGA_INSTANCE} 5672 </dev/null &>/dev/null; do sleep 1; done
 echo "Waiting for Local Message Broker"
-until nc -4 -z ${MQ_INSTANCE} 5672 </dev/null &>/dev/null; do sleep 1; done
+until nc -4 --send-only ${MQ_INSTANCE} 5672 </dev/null &>/dev/null; do sleep 1; done
 
 echo "Starting the verifier"
 ega-verify &
