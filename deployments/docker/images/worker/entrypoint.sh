321cff5530263ef9dd55388517d29ce0779c6f8e
Dmytro Titov <dmytro.titov@gmail.com>
2018-05-25 12:00:15 +0200

    Add gosu to worker and vault.

--- deployments/docker/images/worker/entrypoint.sh
+++ deployments/docker/images/worker/entrypoint.sh
@@ -12,4 +12,4 @@ echo "Waiting for Local Message Broker"
 until nc -4 --send-only ${MQ_INSTANCE} 5672 </dev/null &>/dev/null; do sleep 1; done
 
 echo "Starting the ingestion worker"
-exec ega-ingest
+exec gosu lega ega-ingest
