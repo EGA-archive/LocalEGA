311e5f7682076c24e9c665a3e940942b0c9f1617
Frédéric Haziza <Frederic.Haziza@nbis.se>
2018-05-27 20:11:30 +0200

    PGP keyserver

--- lega/keyserver.py
+++ lega/keyserver.py
@@ -16,12 +16,8 @@ from pathlib import Path
 import ssl
 import struct
 
-from .openpgp.utils import unarmor
-from .openpgp.packet import iter_packets
 from .conf import CONF, KeysConfiguration
 from .utils import get_file_content, db
-from .utils.crypto import get_rsa_private_key_material, serialize_rsa_private_key
-# from .openpgp.generate import generate_pgp_key
 from .utils.eureka import EurekaClient
 
 LOG = logging.getLogger('keyserver')
@@ -38,7 +34,7 @@ class Cache:
         self._FMT = '%d/%b/%y %H:%M:%S'
 
     def set(self, key, value, ttl=None):
-        """Assign in the store to the the key the value, its ttl, and if it is active."""
+        """Assign in the store to the the key the value, its ttl."""
         self._check_limit()
         ttl = self._ttl if not ttl else self._parse_date_time(ttl)
         self._store[key] = (value, ttl)
@@ -62,7 +58,7 @@ class Cache:
             value, expire = data
             if expire and time.time() < expire:
                 keys.append({"keyID": key, "ttl": self._time_delta(expire)})
-            if expire is None and key not in ('active_pgp_key', 'active_rsa_key'):
+            if expire is None:
                 keys.append({"keyID": key, "ttl": "Expiration not set."})
         return keys
 
@@ -94,295 +90,50 @@ class Cache:
         self._store = dict()
 
 
-# All the cache goes here
-_pgp_cache = Cache() # keys are uppercase
-_rsa_cache = Cache()
-_tmp_cache = Cache()
-
-
-class PGPPrivateKey:
-    """The Private PGP key loading."""
-
-    def __init__(self, path, passphrase):
-        """Intialise PrivateKey."""
-        self.path = path
-        assert( isinstance(passphrase,str) )
-        self.passphrase = passphrase.encode()
-        self.key_id = None
-        self.fingerprint = None
-
-    def load_key(self):
-        """Load key and return tuple for reconstruction."""
-        data = None
-        with open(self.path, 'rb') as infile:
-            for packet in iter_packets(unarmor(infile)):
-                LOG.info(str(packet))
-                if packet.tag == 5:
-                    data = packet.unlock(self.passphrase)
-                    self.key_id = packet.key_id
-                else:
-                    packet.skip()
-
-        return (self.key_id.upper(), data)
-
-
-class ReEncryptionKey:
-    """ReEncryption currently done with a RSA key."""
-
-    def __init__(self, key_id, path, passphrase):
-        """Intialise PrivateKey."""
-        self.path = path
-        self.key_id = key_id
-        self.passphrase = passphrase
-
-    def load_key(self):
-        """Load key and unlocks it."""
-        with open(self.path, 'rb') as infile:
-            data = get_rsa_private_key_material(infile.read(), password=self.passphrase)
-            data['id'] = self.key_id
-            return (self.key_id, data)
-
-
-###############################
-## Temp endpoint
-###############################
-@routes.get('/temp/rsa/{requested_id}')
-async def temp_key(request):
-    """Returns the unprotected file content"""
-    requested_id = request.match_info['requested_id']
-    LOG.debug(f'Requested raw rsa keyfile with ID {requested_id}')
-    value = _tmp_cache.get(requested_id)
-    if value:
-        return web.Response(text=value.hex())
-    else:
-        LOG.warn(f"Requested raw keyfile for {requested_id} not found.")
-        return web.HTTPNotFound()
+_cache = Cache() # keys are uppercase
+_active = None
 
 ####################################
 # Caching the keys
 ####################################
 
-async def activate_key(key_name, data):
+async def activate_key(data):
     """(Re)Activate a key."""
-    LOG.debug(f'(Re)Activating a {key_name}')
-    if key_name.startswith("pgp"):
-        obj_key = PGPPrivateKey(data.get('path'), data.get('passphrase'))
-        _cache = _pgp_cache
-    elif key_name.startswith("rsa"):
-        obj_key = ReEncryptionKey(key_name, data.get('path'), data.get('passphrase',None))
-        _cache = _rsa_cache
-        
-        ### Temporary
-        with open(data.get('path'), 'rb') as infile:
-            _tmp_cache.set(key_name,
-                           serialize_rsa_private_key(infile.read(), password=data.get('passphrase',None))
-            )
-
-    else:
-        LOG.error(f"Unrecognised key type: {key_name}")
-        return
-
-    key_id, value = obj_key.load_key()
-    LOG.debug(f'Caching key: {key_id} | {key_name} in the {_cache} cache')
-    if key_name == _pgp_cache.get("active_pgp_key"):
-        _cache.set("active_pgp_key", key_id)
-    _cache.set(key_id, value, ttl=data.get('expire', None))
+    key, _ = pgpy.PGPKey.from_file(data.get('path'))
+    with key.unlock(data.get('passphrase')) as k:
+        key_id = k.fingerprint.keyid.upper()
+        LOG.debug(f'Activating key: {key_id}')
+        _cache.set(key_id, k, ttl=data.get('expire', None))
 
 ####################################
 # Retrieve the active keys
 ####################################
 
 @routes.get('/active/{key_type}')
-async def active_key(request):
-    """Returns a JSON-formated list of numbers to reconstruct the active key, unlocked.
-
-    For PGP key types:
-
-        * The JOSN response contains a "type" attribute to specify which key it is.
-        * If type is "rsa", the public and private attributes contain ('n','e') and ('d','p','q','u') respectively.
-        * If type is "dsa", the public and private attributes contain ('p','q','g','y') and ('x') respectively.
-        * If type is "elg", the public and private attributes contain ('p','g','y') and ('x') respectively.
-
-    For RSA the public and private parts are retrieved in hex format.
-
-    Other key types are not supported.
-    """
+async def retrieve_active_key(request):
     key_type = request.match_info['key_type'].lower()
-    if key_type == 'pgp':
-        active_key = "active_pgp_key"
-        _cache = _pgp_cache
-    elif key_type == 'rsa':
-        active_key = "active_rsa_key"
-        _cache = _rsa_cache
+    LOG.debug(f'Requesting active ({key_type}) key')
+    k = _cache.get(_active)
+    if k:
+        value = k.pubkey if key_type == 'public' else k
+        return web.json_response(bytes(value))
     else:
-        return web.HTTPBadRequest()
-    key_id = _cache.get(active_key)
-    LOG.debug(f'Requesting active %s key', key_type.upper())
-    value = _cache.get(key_id)
-    if value:
-        return web.json_response(value)
-    else:
-        LOG.warn(f"Active key not found.")
+        LOG.warn(f"Requested active ({key_type}) key not found.")
         return web.HTTPNotFound()
-
-
-@routes.get('/active/{key_type}/private')
-async def active_key_private(request):
-    """Retrieve private part to reconstruced unlocked active key.
-
-    For PGP key types:
-
-        * The JOSN response contains a "type" attribute to specify which key it is.
-        * If type is "rsa", the private attribute contains ('d','p','q','u').
-        * If type is "dsa", the private attribute contains ('x').
-        * If type is "elg", the private attribute contains ('x').
-
-    For RSA the public and private parts are retrieved in hex format.
-
-    Other key types are not supported.
-    """
-    key_type = request.match_info['key_type'].lower()
-    if key_type == 'pgp':
-        active_key = "active_pgp_key"
-        _cache = _pgp_cache
-    elif key_type == 'rsa':
-        active_key = "active_rsa_key"
-        _cache = _rsa_cache
-    else:
-        return web.HTTPBadRequest()
-    key_id = _cache.get(active_key)
-    LOG.debug(f'Requesting active %s (private) key', key_type.upper())
-    value = dict(_cache.get(key_id)) if key_id else None  # Avoid 500 server error
-    if value:
-        del value['public']
-        return web.json_response(value)
-    else:
-        LOG.warn(f"Requested active %s (private) key not found.", key_type.upper())
-        return web.HTTPNotFound()
-
-
-@routes.get('/active/{key_type}/public')
-async def active_key_public(request):
-    """Retrieve public to reconstruced unlocked active key.
-
-    For PGP key types:
-
-        * The JOSN response contains a "type" attribute to specify which key it is.
-        * If type is "rsa", the public attribute contains ('n','e').
-        * If type is "dsa", the public attribute contains ('p','q','g','y').
-        * If type is "elg", the public attribute contains ('p','g','y').
-
-    For RSA the public part is retrieved in hex format.
-
-    Other key types are not supported.
-    """
-    key_type = request.match_info['key_type'].lower()
-    if key_type == 'pgp':
-        active_key = "active_pgp_key"
-        _cache = _pgp_cache
-    elif key_type == 'rsa':
-        active_key = "active_rsa_key"
-        _cache = _rsa_cache
-    else:
-        return web.HTTPBadRequest()
-    key_id = _cache.get(active_key)
-    LOG.debug(f'Requesting active %s (public) key', key_type.upper())
-    value = dict(_cache.get(key_id)) if key_id else None  # Avoid 500 server error
-    if value:
-        del value['private']
-        return web.json_response(value)
-    else:
-        LOG.warn(f"Requested %s key (public) not found.", key_type.upper())
-        return web.HTTPNotFound()
-
-
-# Just want to get a key by its key_id PGP or RSA
 
 @routes.get('/retrieve/{key_type}/{requested_id}')
 async def retrieve_key(request):
-    """Returns a JSON-formated list of numbers to reconstruct an unlocked key.
-
-    For PGP key types:
-
-        * The JOSN response contains a "type" attribute to specify which key it is.
-        * If type is "rsa", the public and private attributes contain ('n','e') and ('d','p','q','u') respectively.
-        * If type is "dsa", the public and private attributes contain ('p','q','g','y') and ('x') respectively.
-        * If type is "elg", the public and private attributes contain ('p','g','y') and ('x') respectively.
-
-    For RSA the public and private parts are retrieved in hex format.
-
-    Other key types are not supported.
-    """
     requested_id = request.match_info['requested_id']
     key_type = request.match_info['key_type'].lower()
-    if key_type == 'pgp':
-        _cache = _pgp_cache
-        key_id = requested_id[-16:].upper()
-    elif key_type == 'rsa':
-        _cache = _rsa_cache
-        key_id = requested_id
-    else:
-        return web.HTTPBadRequest()
+    key_id = requested_id[-16:].upper()
     LOG.debug(f'Requested {key_type.upper()} key with ID {requested_id}')
-    value = _cache.get(key_id)
-    if value:
+    k = _cache.get(key_id)
+    if k:
+        value = k.pubkey if key_type == 'public' else k
         return web.json_response(value)
     else:
-        LOG.warn(f"Requested {key_type.upper()} key {requested_id} not found.")
+        LOG.warn(f"Requested key {requested_id} not found.")
         return web.HTTPNotFound()
-
-
-@routes.get('/retrieve/{key_type}/{requested_id}/private')
-async def retrieve_key_private(request):
-    """Retrieve private part to reconstruct unlocked key.
-
-    :py:func:`lega.keyserver.active_key_private`
-    """
-    requested_id = request.match_info['requested_id']
-    key_type = request.match_info['key_type'].lower()
-    if key_type == 'pgp':
-        _cache = _pgp_cache
-        key_id = requested_id[-16:].upper()
-    elif key_type == 'rsa':
-        _cache = _rsa_cache
-        key_id = requested_id
-    else:
-        return web.HTTPBadRequest()
-    LOG.debug(f'Requested {key_type.upper()} (private) key with ID {requested_id}')
-    value = dict(_cache.get(key_id)) if _cache.get(key_id) else None  # Avoid 500 server error
-    if value:
-        del value['public']
-        return web.json_response(value)
-    else:
-        LOG.warn(f"Requested {key_type.upper()} (private) key {requested_id} not found.")
-        return web.HTTPNotFound()
-
-
-@routes.get('/retrieve/{key_type}/{requested_id}/public')
-async def retrieve_key_public(request):
-    """Retrieve public part to reconstruct unlocked key.
-
-    :py:func:`lega.keyserver.active_key_public`
-    """
-    requested_id = request.match_info['requested_id']
-    key_type = request.match_info['key_type'].lower()
-    if key_type == 'pgp':
-        _cache = _pgp_cache
-        key_id = requested_id[-16:].upper()
-    elif key_type == 'rsa':
-        _cache = _rsa_cache
-        key_id = requested_id
-    else:
-        return web.HTTPBadRequest()
-    LOG.debug(f'Requested {key_type.upper()} (public) key with ID {requested_id}')
-    value = dict(_cache.get(key_id)) if _cache.get(key_id) else None  # Avoid 500 server error
-    if value:
-        del value['private']
-        return web.json_response(value)
-    else:
-        LOG.warn(f"Requested {key_type.upper()} (public) key {requested_id} not found.")
-        return web.HTTPNotFound()
-
 
 @routes.post('/admin/unlock')
 async def unlock_key(request):
@@ -397,24 +148,6 @@ async def unlock_key(request):
         return web.HTTPAccepted()
     else:
         return web.HTTPBadRequest()
-
-
-# @routes.post('/generate/pgp')
-# async def generate_pgp_key_pair(request):
-#     """Generate PGP key pair"""
-#     key_options = await request.json()
-#     LOG.debug(f'Admin generate PGP key pair: {key_options}')
-#     if all(k in key_options for k in("name", "comment", "email")):
-#         # By default we can return armored
-#         pub_data, sec_data = generate_pgp_key(key_options['name'],
-#                                               key_options['email'],
-#                                               key_options['comment'],
-#                                               key_options.get('passphrase', None))
-#         # TO DO return the key pair or the path where it is stored.
-#         return web.HTTPAccepted()
-#     else:
-#         return web.HTTPBadRequest()
-
 
 @routes.get('/health')
 async def healthcheck(request):
@@ -437,30 +170,12 @@ async def check_ttl(request):
     else:
         return web.HTTPBadRequest()
 
-@routes.get('/temp/file/{file_id}')
-async def id2info(request):
-    """Translate a file_id to a file info"""
-    file_id = request.match_info['file_id']
-    LOG.debug(f'Translation {file_id} to fileinfo')
-    fileinfo = await db.get_fileinfo(request.app['db'], file_id)
-    if fileinfo:
-        filepath, filesize, checksum, algo = fileinfo # unpack
-        return web.json_response({
-            'filepath': filepath,
-            'filesize': filesize,
-            'checksum': checksum,
-            'algo': algo,
-        })
-    raise web.HTTPNotFound(text=f'Dunno anything about a file with id "{file_id}"\n')
-
 async def load_keys_conf(store):
     """Parse and load keys configuration."""
     # Cache the active key names
     for name, value in store.defaults().items():
-        if name == 'pgp':
-            _pgp_cache.set('active_pgp_key', value)
-        if name == 'rsa':
-            _rsa_cache.set('active_rsa_key', value)
+        if name == 'active':
+            _active = value
     # Load all the keys in the store
     for section in store.sections():
         await activate_key(section, dict(store.items(section)))
