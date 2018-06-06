2ee94b3b9fe708d7355dd75c78d6549dba9df3b4
Frédéric Haziza <Frederic.Haziza@nbis.se>
2018-06-07 01:01:40 +0200

    New ingestion with GA4GH

--- lega/verify.py
+++ lega/verify.py
@@ -3,24 +3,41 @@
 
 '''Verifying the vault files
 
-This module reads a message from the ``archived`` queue and
-recalculates the checksum of the requested vault file.
+This module reads a message from the ``staged`` queue, decrypts the
+files and recalculates its checksum.
 
-It the checksum matches the corresponding information in the database,
-we consider that the vault has properly stored the file.
+It the checksum matches the corresponding information in the file,
+we consider that the vault has a properly stored file.
 
 Upon completion, a message is sent to the local exchange with the
 routing key: ``completed``.
+
 '''
 
 import sys
+import os
 import logging
 
+from legacryptor.crypt4gh import get_key_id
+
 from .conf import CONF
-from .utils import checksum, db, exceptions
+from .utils import db, exceptions
 from .utils.amqp import consume
 
-LOG = logging.getLogger('verify')
+LOG = logging.getLogger(__name__)
+
+def get_priv_key(header):
+    try:
+        keyid = get_key_id(header)
+        LOG.info(f'Key ID {keyid}')
+        keyurl = CONF.get_value('quality_control', 'keyserver_endpoint', raw=True) % keyid
+        LOG.info(f'Retrieving the Master Public Key from {keyurl}')
+        with urlopen(keyurl) as response:
+            return response.read()
+    except Exception as e:
+        LOG.error(repr(e))
+        LOG.critical('Problem contacting the Keyserver. Terminating...')
+        sys.exit(1)
 
 @db.catch_error
 def work(data):
@@ -29,10 +46,14 @@ def work(data):
     LOG.debug(f'Verifying message: {data}')
 
     file_id = data.pop('internal_data') # can raise KeyError
-    filename, _, org_hash_algo, vault_filename, stable_id, vault_checksum = db.get_details(file_id)
+    filename, vault_filename, stable_id, header = db.get_details(file_id)
 
-    if not checksum.is_valid(vault_filename, vault_checksum, hashAlgo='sha256'):
-        raise exceptions.VaultDecryption(vault_filename)
+    # Get it from the header and the keyserver
+    privkey = get_priv_key(header)
+
+    # If you can decrypt and 
+    with open(vault_filename, 'rb') as infile, open(os.devnull, 'wb') as outfile:
+        decrypt(infile, outfile, privkey)
 
     data['status'] = { 'state': 'COMPLETED', 'details': stable_id }
     return data
