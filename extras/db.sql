d0b7817915487132afa2709850c41bb624c00493
Frédéric Haziza <Frederic.Haziza@nbis.se>
2018-06-08 21:40:26 +0200

    Verify Checksum as Quality Control.

--- extras/db.sql
+++ extras/db.sql
@@ -3,7 +3,7 @@
 SET TIME ZONE 'Europe/Stockholm';
 
 CREATE TYPE status AS ENUM ('Received', 'In progress', 'Completed', 'Archived', 'Error');
-CREATE TYPE hash_algo AS ENUM ('md5', 'sha256');
+-- CREATE TYPE hash_algo AS ENUM ('md5', 'sha256');
 
 CREATE EXTENSION pgcrypto;
 
@@ -16,24 +16,24 @@ CREATE TABLE files (
 	inbox_path     TEXT NOT NULL,
 	status         status,
 	vault_path     TEXT,
-	vault_filesize Integer,
+	vault_filesize INTEGER,
 	stable_id      TEXT,
 	header         TEXT, -- crypt4gh
 	created_at     TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT clock_timestamp(),
 	last_modified  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT clock_timestamp()
 );
 
-CREATE FUNCTION insert_file(filename    files.filename%TYPE,
-			    eid         files.elixir_id%TYPE,
-			    stable_id   files.stable_id%TYPE,
-			    status      files.status%TYPE)
+CREATE FUNCTION insert_file(inpath files.inbox_path%TYPE,
+			    eid    files.elixir_id%TYPE,
+			    sid    files.stable_id%TYPE,
+			    status files.status%TYPE)
     RETURNS files.id%TYPE AS $insert_file$
     #variable_conflict use_column
     DECLARE
         file_id files.id%TYPE;
     BEGIN
 	INSERT INTO files (inbox_path,elixir_id,stable_id,status)
-	VALUES(filename,eid,stable_id,status) RETURNING files.id
+	VALUES(inpath,eid,sid,status) RETURNING files.id
 	INTO file_id;
 	RETURN file_id;
     END;
