0cbd97a898087c643df89b2f6c6a9109675e71d0
Frédéric Haziza <Frederic.Haziza@nbis.se>
2018-06-08 21:40:26 +0200

    Verify Checksum as Quality Control.

--- deployments/docker/images/qc/entrypoint.sh
+++ deployments/docker/images/qc/entrypoint.sh
@@ -4,19 +4,9 @@ set -e
 
 # MQ_INSTANCE env must be defined
 [[ -z "$MQ_INSTANCE" ]] && echo 'Environment MQ_INSTANCE is empty' 1>&2 && exit 1
-
-# Changing permissions
-echo "Changing permissions for /ega/vault"
-chown lega:lega /ega/vault
-chmod 750 /ega/vault 
-chmod g+s /ega/vault # setgid bit
-
 echo "Waiting for Local Message Broker"
 until nc -4 --send-only ${MQ_INSTANCE} 5672 </dev/null &>/dev/null; do sleep 1; done
 
-echo "Starting the verifier"
+echo "Starting _a_ Quality Controller"
 #exec gosu lega ega-verify
 exec ega-verify
-
-# echo "Starting the vault listener"
-# exec gosu lega ega-vault
--- lega/verify.py
+++ lega/verify.py
@@ -16,30 +16,28 @@ routing key: ``completed``.
 
 import sys
 import logging
+from functools import partial
+from urllib.request import urlopen
 
-from legacryptor.crypt4gh import get_key_id
+from legacryptor.crypt4gh import get_key_id, header_to_records, body_decrypt
 
 from .conf import CONF
-from .utils import db, exceptions
+from .utils import db, exceptions, storage
 from .utils.amqp import consume
 
 LOG = logging.getLogger(__name__)
 
-def get_priv_key(header):
-    try:
-        keyid = get_key_id(header)
-        LOG.info(f'Key ID {keyid}')
-        keyurl = CONF.get_value('quality_control', 'keyserver_endpoint', raw=True) % keyid
-        LOG.info(f'Retrieving the Master Public Key from {keyurl}')
-        with urlopen(keyurl) as response:
-            return response.read()
-    except Exception as e:
-        LOG.error(repr(e))
-        LOG.critical('Problem contacting the Keyserver. Terminating...')
-        sys.exit(1)
+def get_records(header):
+    keyid = get_key_id(header)
+    LOG.info(f'Key ID {keyid}')
+    keyurl = CONF.get('quality_control', 'keyserver_endpoint', raw=True) % keyid
+    LOG.info(f'Retrieving the Private Key from {keyurl}')
+    with urlopen(keyurl) as response:
+        privkey = response.read()
+        return header_to_records(privkey, header)
 
 @db.catch_error
-def work(data):
+def work(mover, data):
     '''Verifying that the file in the vault does decrypt properly'''
 
     LOG.debug(f'Verifying message: {data}')
@@ -48,13 +46,18 @@ def work(data):
     _, vault_path, stable_id, header = db.get_info(file_id)
 
     # Get it from the header and the keyserver
-    privkey = get_priv_key(header)
+    records = get_records(bytes.fromhex(header)) # might raise exception
+    r = records[0] # only first one
 
+    LOG.info(f'Session Key: {r.session_key.hex()}')
+    LOG.info(f'         IV: {r.iv.hex()}')
+    
+    LOG.debug('Opening vault file: %s', vault_path)
     # If you can decrypt... the checksum is valid
-    with open(vault_path, 'rb') as infile:
-        decrypt(privkey, infile) # It will ignore the output
+    with mover.open(vault_path, 'rb') as infile:
+        body_decrypt(r, infile) # It will ignore the output
 
-    db.set_status(file_id, 'Completed')
+    db.set_status(file_id, db.Status.Completed)
     data['status'] = { 'state': 'COMPLETED', 'details': stable_id }
     LOG.debug(f"Reply message: {data}")
     return data
@@ -66,7 +69,10 @@ def main(args=None):
 
     CONF.setup(args) # re-conf
 
-    consume(work, 'staged', 'completed')
+    store = getattr(storage, CONF.get('vault', 'driver', fallback='FileStorage'))
+    do_work = partial(work, store())
+
+    consume(do_work, 'staged', 'completed')
 
 if __name__ == '__main__':
     main()
