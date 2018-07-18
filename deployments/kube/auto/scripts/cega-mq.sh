03027a6d0763455e9e7739dfcccec3372d4e379f
Stefan Negru <stefan@blankdots.com>
2018-07-18 09:52:20 +0300

    addressing changes and adding fake-cega to deploy script

---
+++ deployments/kube/auto/scripts/cega-mq.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# Initialization
+rabbitmq-plugins enable --offline rabbitmq_federation
+rabbitmq-plugins enable --offline rabbitmq_federation_management
+rabbitmq-plugins enable --offline rabbitmq_shovel
+rabbitmq-plugins enable --offline rabbitmq_shovel_management
+
+cp --remove-destination /temp/rabbitmq.config /etc/rabbitmq/rabbitmq.config
+cp --remove-destination /temp/defs.json /etc/rabbitmq/defs.json
+chown rabbitmq:rabbitmq /etc/rabbitmq/rabbitmq.config
+chmod 640 /etc/rabbitmq/rabbitmq.config
+chown rabbitmq:rabbitmq /etc/rabbitmq/defs.json
+chmod 640 /etc/rabbitmq/defs.json
+
+exec rabbitmq-server
