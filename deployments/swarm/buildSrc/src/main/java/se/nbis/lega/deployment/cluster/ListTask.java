1f8b08df58220800c2ac7d26308c2f86f87aff4c
Dmytro Titov <dmytro.titov@gmail.com>
2018-08-01 10:42:00 +0200

    Create workers in parallel using ThreadPool.

--- deployments/swarm/buildSrc/src/main/java/se/nbis/lega/deployment/cluster/ListTask.java
+++ deployments/swarm/buildSrc/src/main/java/se/nbis/lega/deployment/cluster/ListTask.java
@@ -4,7 +4,7 @@ import org.gradle.api.tasks.TaskAction;
 
 import java.io.IOException;
 
-public class LSTask extends ClusterTask {
+public class ListTask extends ClusterTask {
 
     @TaskAction
     public void run() throws IOException {
