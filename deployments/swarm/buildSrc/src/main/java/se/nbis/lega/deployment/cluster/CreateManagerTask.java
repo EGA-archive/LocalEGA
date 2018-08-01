6bf37b81e056a8c665217f6288b81a338b058c5f
Dmytro Titov <dmytro.titov@gmail.com>
2018-08-01 14:30:23 +0200

    Finalize cluster provisioning tasks.

--- deployments/swarm/buildSrc/src/main/java/se/nbis/lega/deployment/cluster/CreateManagerTask.java
+++ deployments/swarm/buildSrc/src/main/java/se/nbis/lega/deployment/cluster/CreateManagerTask.java
@@ -1,32 +1,18 @@
 package se.nbis.lega.deployment.cluster;
 
-import org.gradle.api.internal.tasks.options.Option;
-import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
 public class CreateManagerTask extends ClusterTask {
 
-    private String openStackConfig;
-
     @TaskAction
     public void run() throws IOException {
+        String openStackConfig = getProperty("openStackConfig");
         Map<String, String> env = createMachine(MANAGER_NAME, openStackConfig);
         String machineIPAddress = getMachineIPAddress(MANAGER_NAME);
         exec(true, env, "docker swarm init", "--advertise-addr", machineIPAddress);
-    }
-
-    @Option(option = "openStackConfig", description = "Path to file with OpenStack configuration")
-    public void setOpenStackConfig(String openStackConfig) {
-        this.openStackConfig = openStackConfig;
-    }
-
-    @OutputFile
-    public File getTraceFile() {
-        return getProject().file(".tmp");
     }
 
 }
