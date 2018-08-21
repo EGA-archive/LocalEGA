e4ac6b2c1176f08de4c8e7574b169639e04796fc
Frédéric Haziza <frederic.haziza@nbis.se>
2018-08-21 08:23:18 +0200

    Renaming inbox script into notifications

--- docker/images/inbox/entrypoint.sh
+++ docker/images/inbox/entrypoint.sh
@@ -42,7 +42,7 @@ chmod 750 /ega/inbox
 chmod g+s /ega/inbox # setgid bit
 
 echo "Starting the FileSystem listener"
-gosu lega ega-inbox &
+gosu lega ega-notifications &
 
 echo "Starting the SFTP server"
 exec /opt/openssh/sbin/ega -D -e -f /etc/ega/sshd_config
