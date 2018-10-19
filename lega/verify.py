9e735e444a96a2a762fabd88f111001a393adfde
Johan Viklund <johan.viklund@nbis.se>
2018-10-19 13:30:35 +0200

    OOPified verify worker

--- lega/verify.py
+++ lega/verify.py
@@ -14,7 +14,6 @@ with the routing key: ``completed``.
 import sys
 import os
 import logging
-from functools import partial
 from urllib.request import urlopen
 from urllib.error import HTTPError
 import hashlib
@@ -24,81 +23,81 @@ from legacryptor.crypt4gh import get_key_id, header_to_records, body_decrypt
 from .conf import CONF
 from .utils import db, exceptions, storage
 from .utils.amqp import consume, get_connection
+from .utils.worker import Worker
 
 LOG = logging.getLogger(__name__)
 
 
-def get_records(header):
-    """Retrieve Crypt4GH header information (records) from Keyserver."""
-    keyid = get_key_id(header)
-    LOG.info(f'Key ID {keyid}')
-    keyurl = CONF.get_value('quality_control', 'keyserver_endpoint', raw=True) % keyid
-    verify = CONF.get_value('quality_control', 'verify_certificate', conv=bool)
-    LOG.info(f'Retrieving the Private Key from {keyurl} (verify certificate: {verify})')
+class VerifyWorker(Worker):
+    def do_work(self, chunk_size, mover, channel, data):
+        """Verify that the file in the vault can be properly decrypted."""
+        LOG.info('Verification | message: %s', data)
 
-    if verify:
-        ctx = None  # nothing to be done: done by default in urlopen
-    else:  # no verification
-        import ssl
-        ctx = ssl.create_default_context()
-        ctx.check_hostname = False
-        ctx.verify_mode = ssl.CERT_NONE
+        file_id = data['file_id']
+        header = bytes.fromhex(data['header'])[16:]  # in hex -> bytes, and take away 16 bytes
+        vault_path = data['vault_path']
 
-    try:
-        with urlopen(keyurl, context=ctx) as response:
-            privkey = response.read()
-            return header_to_records(privkey, header, os.environ['LEGA_PASSWORD']), keyid
-    except HTTPError as e:
-        LOG.error(e)
-        msg = str(e)
-        if e.code == 404:  # If key not found, then probably wrong key.
-            raise exceptions.PGPKeyError(msg)
-        # Otherwise
-        raise exceptions.KeyserverError(msg)
-    except Exception as e:
-        raise exceptions.KeyserverError(str(e))
+        # Get it from the header and the keyserver
+        records, key_id = self.get_records(header)  # might raise exception
+        r = records[0]  # only first one
 
+        LOG.info('Opening vault file: %s', vault_path)
+        # If you can decrypt... the checksum is valid
 
-@db.catch_error
-@db.crypt4gh_to_user_errors
-def work(chunk_size, mover, channel, data):
-    """Verify that the file in the vault can be properly decrypted."""
-    LOG.info('Verification | message: %s', data)
+        # Calculate the checksum of the original content
+        md = hashlib.sha256()
 
-    file_id = data['file_id']
-    header = bytes.fromhex(data['header'])[16:]  # in hex -> bytes, and take away 16 bytes
-    vault_path = data['vault_path']
+        def checksum_content(data):
+            md.update(data)
 
-    # Get it from the header and the keyserver
-    records, key_id = get_records(header)  # might raise exception
-    r = records[0]  # only first one
+        with mover.open(vault_path, 'rb') as infile:
+            LOG.info('Decrypting (chunk size: %s)', chunk_size)
+            body_decrypt(r, infile, process_output=checksum_content, chunk_size=chunk_size)
 
-    LOG.info('Opening vault file: %s', vault_path)
-    # If you can decrypt... the checksum is valid
+        digest = md.hexdigest()
+        LOG.info('Verification completed [sha256: %s]', digest)
 
-    # Calculate the checksum of the original content
-    md = hashlib.sha256()
+        # Updating the database
+        db.mark_completed(file_id)
 
-    def checksum_content(data):
-        md.update(data)
+        # Shape successful message
+        org_msg = data['org_msg']
+        org_msg.pop('file_id', None)
+        org_msg['reference'] = file_id
+        org_msg['checksum'] = {'value': digest, 'algorithm': 'sha256'}
+        LOG.debug(f"Reply message: {org_msg}")
+        return org_msg
 
-    with mover.open(vault_path, 'rb') as infile:
-        LOG.info('Decrypting (chunk size: %s)', chunk_size)
-        body_decrypt(r, infile, process_output=checksum_content, chunk_size=chunk_size)
+    def get_records(self, header):
+        """Retrieve Crypt4GH header information (records) from Keyserver."""
+        keyid = get_key_id(header)
+        LOG.info(f'Key ID {keyid}')
+        keyurl = CONF.get_value('quality_control', 'keyserver_endpoint', raw=True) % keyid
+        verify = CONF.get_value('quality_control', 'verify_certificate', conv=bool)
+        LOG.info(f'Retrieving the Private Key from {keyurl} (verify certificate: {verify})')
 
-    digest = md.hexdigest()
-    LOG.info('Verification completed [sha256: %s]', digest)
+        if verify:
+            ctx = None  # nothing to be done: done by default in urlopen
+        else:  # no verification
+            import ssl
+            ctx = ssl.create_default_context()
+            ctx.check_hostname = False
+            ctx.verify_mode = ssl.CERT_NONE
 
-    # Updating the database
-    db.mark_completed(file_id)
+        try:
+            with urlopen(keyurl, context=ctx) as response:
+                privkey = response.read()
+                return header_to_records(privkey, header, os.environ['LEGA_PASSWORD']), keyid
+        except HTTPError as e:
+            LOG.error(e)
+            msg = str(e)
+            if e.code == 404:  # If key not found, then probably wrong key.
+                raise exceptions.PGPKeyError(msg)
+            # Otherwise
+            raise exceptions.KeyserverError(msg)
+        except Exception as e:
+            raise exceptions.KeyserverError(str(e))
 
-    # Shape successful message
-    org_msg = data['org_msg']
-    org_msg.pop('file_id', None)
-    org_msg['reference'] = file_id
-    org_msg['checksum'] = {'value': digest, 'algorithm': 'sha256'}
-    LOG.debug(f"Reply message: {org_msg}")
-    return org_msg
 
 
 def main(args=None):
@@ -108,11 +107,13 @@ def main(args=None):
 
     CONF.setup(args)  # re-conf
 
+    worker = VerifyWorker()
+
     store = getattr(storage, CONF.get_value('vault', 'driver', default='FileStorage'))
     chunk_size = CONF.get_value('vault', 'chunk_size', conv=int, default=1 << 22)  # 4 MB
 
     broker = get_connection('broker')
-    do_work = partial(work, chunk_size, store(), broker.channel())
+    do_work = worker.worker(chunk_size, store(), broker.channel())
 
     consume(do_work, broker, 'archived', 'completed')
 
