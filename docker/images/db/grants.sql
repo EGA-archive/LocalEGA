52145b8c13fe1003a7b7632f9e5f07828b40b6db
Stefan Negru <stefan@blankdots.com>
2018-10-25 09:46:05 +0300

    adding tables for ebi

--- docker/images/db/grants.sql
+++ docker/images/db/grants.sql
@@ -16,8 +16,12 @@ CREATE USER lega_out;
 GRANT USAGE ON SCHEMA local_ega TO lega_in, lega_out;
 GRANT ALL PRIVILEGES ON ALL TABLES    IN SCHEMA local_ega TO lega_in; -- Read/Write access on local_ega.* for lega_in
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA local_ega TO lega_in; -- Don't forget the sequences
-GRANT SELECT ON local_ega.vault_files TO lega_out;                    -- Read-Only access for lega_out
-GRANT SELECT ON local_ega.ebi_files   TO lega_out;                    -- Used by EBI
+GRANT SELECT ON local_ega.vault_files  TO lega_out;                    -- Read-Only access for lega_out
+GRANT SELECT ON local_ega.ebi_files    TO lega_out;                    -- Used by EBI
+GRANT SELECT ON local_ega.index_files  TO lega_out;                    -- Used by EBI
+GRANT SELECT ON local_ega.file2dataset TO lega_out;                    -- Used by EBI
+GRANT SELECT ON local_ega.event        TO lega_out;                    -- Used by EBI
+GRANT SELECT ON local_ega.download_log TO lega_out;                    -- Used by EBI
 
 -- Set up rights access for audit schema
 GRANT USAGE ON SCHEMA local_ega_download TO lega_out;
