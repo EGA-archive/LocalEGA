9debdf15eb603b9c5a7e1b487f724e3e544198ec
Frédéric Haziza <frederic.haziza@crg.eu>
2019-02-11 08:26:44 +0100

    Pinning the same version for the S3-backed inbox.

--- deploy/bootstrap/run.sh
+++ deploy/bootstrap/run.sh
@@ -533,7 +533,7 @@ cat >> ${PRIVATE}/lega.yml <<EOF
     container_name: inbox-s3-backend
     labels:
         lega_label: "inbox-s3-backend"
-    image: minio/minio
+    image: minio/minio:RELEASE.2018-12-19T23-46-24Z
     environment:
       - MINIO_ACCESS_KEY=${S3_ACCESS_KEY_INBOX}
       - MINIO_SECRET_KEY=${S3_SECRET_KEY_INBOX}
