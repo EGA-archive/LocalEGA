a5659938a84ed584c7ca7b6cc705d08b56ce2cc1
Frédéric Haziza <frederic.haziza@crg.eu>
2019-11-28 00:20:46 +0100

    Fixing some unit tests

--- tests/unit/test_ingest.py
+++ tests/unit/test_ingest.py
@@ -3,7 +3,7 @@ from lega.ingest import main, work
 from unittest import mock
 from testfixtures import tempdir
 # from pathlib import PosixPath
-from . import pgp_data
+from . import c4gh_data
 from lega.utils.exceptions import FromUser
 
 
@@ -28,20 +28,20 @@ class testIngest(unittest.TestCase):
     def test_work(self, mock_db, mock_header, filedir):
         """Test ingest worker, should send a messge."""
         # Mocking a lot of stuff, as it is previously tested
-        mock_header.return_value = b'beginning', b'header'
+        mock_header.return_value = b'header'
         mock_db.insert_file.return_value = 32
         store = mock.MagicMock()
         store.location.return_value = 'smth'
         store.open.return_value = mock.MagicMock()
         mock_broker = mock.MagicMock(name='channel')
         mock_broker.channel.return_value = mock.Mock()
-        infile = filedir.write('infile.in', bytearray.fromhex(pgp_data.ENC_FILE))
+        infile = filedir.write('infile.in', bytearray.fromhex(c4gh_data.ENC_FILE))
         data = {'filepath': infile, 'user': 'user_id@elixir-europe.org'}
         result = work(store, store, mock_broker, data)
         mocked = {'filepath': infile, 'user': 'user_id@elixir-europe.org',
                   'file_id': 32,
                   'org_msg': {'filepath': infile, 'user': 'user_id@elixir-europe.org'},
-                  'header': '626567696e6e696e67686561646572',
+                  'header': '686561646572',
                   'archive_path': 'smth'}
         self.assertEqual(mocked, result)
         filedir.cleanup()
@@ -52,7 +52,7 @@ class testIngest(unittest.TestCase):
     def test_db_fail(self, mock_db, mock_header, filedir):
         """Test ingest worker, insert_file fails."""
         # Mocking a lot of stuff, as it is previously tested
-        mock_header.return_value = b'beginning', b'header'
+        mock_header.return_value = b'header'
         mock_db.insert_file.side_effect = Exception("Some strange exception")
 
         store = mock.MagicMock()
@@ -60,7 +60,7 @@ class testIngest(unittest.TestCase):
         store.open.return_value = mock.MagicMock()
         mock_broker = mock.MagicMock(name='channel')
         mock_broker.channel.return_value = mock.Mock()
-        infile = filedir.write('infile.in', bytearray.fromhex(pgp_data.ENC_FILE))
+        infile = filedir.write('infile.in', bytearray.fromhex(c4gh_data.ENC_FILE))
 
         data = {'filepath': infile, 'user': 'user_id@elixir-europe.org'}
         result = work(store, mock_broker, data)
@@ -74,7 +74,7 @@ class testIngest(unittest.TestCase):
     def test_mark_in_progress_fail(self, mock_set_error, mock_db, mock_header, filedir):
         """Test ingest worker, mark_in_progress fails."""
         # Mocking a lot of stuff, as it is previously tested
-        mock_header.return_value = b'beginning', b'header'
+        mock_header.return_value = b'header'
         mock_db.mark_in_progress.side_effect = Exception("Some strange exception")
 
         store = mock.MagicMock()
@@ -82,7 +82,7 @@ class testIngest(unittest.TestCase):
         store.open.return_value = mock.MagicMock()
         mock_broker = mock.MagicMock(name='channel')
         mock_broker.channel.return_value = mock.Mock()
-        infile = filedir.write('infile.in', bytearray.fromhex(pgp_data.ENC_FILE))
+        infile = filedir.write('infile.in', bytearray.fromhex(c4gh_data.ENC_FILE))
 
         data = {'filepath': infile, 'user': 'user_id@elixir-europe.org'}
         result = work(store, store, mock_broker, data)
@@ -99,7 +99,7 @@ class testIngest(unittest.TestCase):
     def test_mark_in_progress_fail_with_from_user_error(self, mock_publish, mock_get_connection, mock_set_error, mock_db, mock_header, filedir):
         """Test ingest worker, mark_in_progress fails."""
         # Mocking a lot of stuff, as it is previously tested
-        mock_header.return_value = b'beginning', b'header'
+        mock_header.return_value = b'header'
         mock_db.mark_in_progress.side_effect = FromUser()
 
         store = mock.MagicMock()
@@ -107,7 +107,7 @@ class testIngest(unittest.TestCase):
         store.open.return_value = mock.MagicMock()
         mock_broker = mock.MagicMock(name='channel')
         mock_broker.channel.return_value = mock.Mock()
-        infile = filedir.write('infile.in', bytearray.fromhex(pgp_data.ENC_FILE))
+        infile = filedir.write('infile.in', bytearray.fromhex(c4gh_data.ENC_FILE))
 
         data = {'filepath': infile, 'user': 'user_id@elixir-europe.org'}
         result = work(store, store, mock_broker, data)
