d299f29914a189653b4ea080fae9226667d5cb9d
Stefan Negru <stefan@blankdots.com>
2019-11-29 14:06:11 +0200

    adjust test helper scripts for new certificates

--- tests/_common/helpers.bash
+++ tests/_common/helpers.bash
@@ -23,9 +23,8 @@ USERS_FILE=${DOCKER_PATH}/private/.users
 export CEGA_CONNECTION="amqps://legatest:legatest@localhost:5670/lega"
 
 # Create certfile/keyfile for testsuite
-#yes | make --silent -C ${MAIN_REPO}/deploy/bootstrap/certs testsuite OPENSSL=${OPENSSL:-openssl} &>/dev/null
-cp -f ${MAIN_REPO}/deploy/bootstrap/certs/data/testsuite.{cert,sec}.pem ${HERE}/mq/.
-cp -f ${MAIN_REPO}/deploy/bootstrap/certs/data/CA.cert.pem ${HERE}/mq/.
+cp -f ${MAIN_REPO}/deploy/private/config/certs/tester.ca.{key,crt} ${HERE}/mq/.
+cp -f ${MAIN_REPO}/deploy/private/config/certs/root.ca.crt ${HERE}/mq/.
 
 # Utilities to scan the Message Queues
 MQ_CONSUME="python ${HERE}/mq/consume.py --connection ${CEGA_CONNECTION}"
