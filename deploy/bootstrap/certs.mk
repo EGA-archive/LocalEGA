d6037abcefa81162549b71bb4d090ea351b83886
Frédéric Haziza <frederic.haziza@crg.eu>
2020-01-02 14:27:44 +0100

    Cleaner Makefiles and dependencies

--- deploy/bootstrap/certs.mk
+++ deploy/bootstrap/certs.mk
@@ -5,71 +5,29 @@ SUBJ_PREFIX := /C=ES/ST=Spain/L=Barcelona/O=CRG/OU=EGA
 # HOSTNAME_DOMAIN # defined in the including Makefile
 DOMAIN_EMAIL := -dev.ega@crg.eu
 
-.PHONY: all-certs prepare-certs clean-certs cega-certs
-
-COMPONENTS=ingest \
-           verify \
-           finalize \
-           db \
-           mq \
-           keys \
-           inbox \
-	   outgest \
-	   streamer
-
-ifdef S3
-COMPONENTS+=archive inbox-s3-backend
-endif
-
-CEGA_COMPONENTS=cega-mq cega-users
-
-ALL_COMPONENTS=$(COMPONENTS) $(CEGA_COMPONENTS) testsuite
-
-.PRECIOUS: $(OPENSSL_DBs)                                 \
-	   $(ALL_COMPONENTS:%=../private/certs/%.sec.pem) \
-	   $(ALL_COMPONENTS:%=../private/certs/%.csr.pem)
-
-#.INTERMEDIATE: ../private/certs/*.csr.pem
-
-all-certs: prepare-certs \
-	   $(COMPONENTS:%=../private/certs/%.cert.pem) \
-	   $(COMPONENTS:%=../private/certs/CA.%.cert.pem)
-	-@chmod 444 $(COMPONENTS:%=../private/certs/%.sec.pem)
-
-testsuite-certs: prepare-certs \
-	   	 ../private/certs/testsuite.cert.pem \
-	         ../private/certs/CA.testsuite.cert.pem
-
-cega-certs: prepare-certs \
-	   $(CEGA_COMPONENTS:%=../private/certs/%.cert.pem) \
-	   $(CEGA_COMPONENTS:%=../private/certs/CA.%.cert.pem)
-	-@chmod 444 $(CEGA_COMPONENTS:%=../private/certs/%.sec.pem)
-
 ###############################################
 ## Necessary files
 ###############################################
 
-# We add a separate rules and not a dependency,
-# because these files will change for each created certificate
-# and we don't want to re-create *.cert.pem for that
-prepare-certs:
-	@mkdir -p ../private/certs
-	@touch ../private/certs/index.txt
-	@touch ../private/certs/index.txt.attr
-	@test -e ../private/certs/serial || echo 1000 > ../private/certs/serial
+../private/certs:
+	@mkdir -p $@
 
+../private/certs/serial: | ../private/certs
+	@echo '1000' > $@
+
+../private/certs/index.txt: | ../private/certs
+	@touch $@
 
 ###############################################
 ## Root CA Certificates
 ###############################################
 
-../private/certs/CA.cert.pem: ../private/certs/CA.sec.pem
+../private/certs/CA.cert.pem: ../private/certs/CA.sec.pem | ../private/certs
 	@echo "Creating $(@F)"
-	@mkdir -p $(@D) 
 	@$(OPENSSL) req -config certs.cnf \
 	           -subj "/C=ES/ST=Spain/L=Barcelona/O=CRG/OU=EGA/CN=LocalEGA root CA/emailAddress=dev.ega@crg.eu" \
 	           -key $< -x509 -new -days 7300 -sha256 -nodes \
-	           -extensions v3_ca -out $@
+	           -extensions v3_ca -out $@ 2>../private/.err
 
 
 ###############################################
@@ -94,37 +52,42 @@ ifdef S3
 ../private/certs/inbox-s3-backend.cert.pem: EXT=server_cert
 endif
 
-%.cert.pem: %.csr.pem
+%.cert.pem: %.csr.pem | ../private/certs/serial ../private/certs/index.txt
 	@echo "Creating $(@F)"
-	@$(OPENSSL) ca -config certs.cnf \
+	@yes | $(OPENSSL) ca -config certs.cnf \
 	           -extensions $(EXT) \
-	           -days 375 -notext -md sha256 -in $< -out $@
-#	-@rm ../private/certs/100*.pem
+	           -days 375 -notext -md sha256 -in $< -out $@ 2>../private/.err
+	-@rm ../private/certs/10*.pem
 
 ###############################################
 ## CSR - Certificate Signing Request
 ###############################################
+# Uncomment if you want to keep the CSRs
+# .PRECIOUS: %.csr.pem
 
-%.csr.pem: %.sec.pem ../private/certs/CA.cert.pem
+%.csr.pem: %.sec.pem ../private/certs/CA.cert.pem | ../private/certs
 	@echo "Creating $(@F)"
 	@$(OPENSSL) req -config certs.cnf \
 	           -subj "${SUBJ_PREFIX}/CN=$(@:../private/certs/%.csr.pem=%)${HOSTNAME_DOMAIN}/emailAddress=$(@:../private/certs/%.csr.pem=%)${DOMAIN_EMAIL}" \
-	           -key $< -new -sha256 -out $@
+	           -key $< -new -sha256 -out $@ 2>../private/.err
 
 
 ###############################################
 ## Private keys
 ###############################################
-../private/certs/%.sec.pem:
+../private/certs/%.sec.pem: | ../private/certs
 	@echo "Creating $(@F)"
-	@mkdir -p $(@D) 
-	@$(OPENSSL) genpkey -algorithm RSA -out $@
-	@chmod 400 $@
+	@$(OPENSSL) genpkey -algorithm RSA -out $@ 2>../private/.err
+	@chmod 444 $@
+
+# 444 instead of 400 to solve permission issues when docker-compose is injecting the files
+# Normally not happening when using docker secrets
 
 ###############################################
 ## Misc
 ###############################################
 
+.PHONY: clean-certs
 clean-certs:
 	rm -rf ../private/certs
 
--- deploy/bootstrap/users.mk
+++ deploy/bootstrap/users.mk
@@ -1,24 +1,27 @@
-prepare-users:
-	@mkdir -p ../private/users
-	@test -e ../private/users/serial || echo '15000' > ../private/users/serial
+../private/users:
+	@mkdir -p $@
 
-../private/users/%.sshkey: ../private/users/%.passphrase
-	@echo "Creating $(@F)"
+../private/users/serial: | ../private/users
+	@echo '15000' > $@
+
+../private/users/%.sshkey: ../private/users/%.passphrase  | ../private/users
+	@echo "Creating user keypair: $(@F)"
 	@ssh-keygen -t ed25519 -f $@ -N "$(file < $<)" -C "$(@:../private/users/%.sshkey=%)@LocalEGA" &>/dev/null
 	@chmod 400 $@
 # Making the key readable, because when it's injected inside the container
 # it retains the permissions. Therefore, originally 400, will make it unreadable to the lega user.
 
-../private/users/%.json: ../private/users/%.sshkey
-	@mkdir -p $(@D)
-	@echo "Creating $(@F)"
+../private/users/%.json: ../private/users/%.sshkey | ../private/users/serial
+	@echo "Creating user file: $(@F)"
 	@python -m run.cega.user $(@:../private/users/%.json=%) $(file < ../private/users/serial) '$(@:.json=.passphrase)' < $(<:=.pub) > $@
 	@$(file > ../private/users/serial,$(shell echo "$$(($(file < ../private/users/serial)+1))"))
 
-../private/users/%.passphrase:
-	@mkdir -p $(@D)
-	@echo "Creating $(@F)"
+../private/users/%.passphrase: | ../private/users
+	@echo "Creating user passphrase: $(@F)"
 	@python -m run.pwd_gen 8 > $@
 
+../private/users/%:
+
+.PHONY: clean-users
 clean-users:
 	rm -rf ../private/users
