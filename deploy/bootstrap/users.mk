4366a3cbcb872729f08d3111f6a5e76e7847cded
Frédéric Haziza <frederic.haziza@crg.eu>
2020-01-03 03:21:08 +0100

    Make 4.2.1 not available on Github Actions runner, so $(file <) and $(file >) do not work. Fixing it with $(shell cat)

--- deploy/bootstrap/users.mk
+++ deploy/bootstrap/users.mk
@@ -6,15 +6,15 @@
 
 ../private/users/%.sshkey: ../private/users/%.passphrase  | ../private/users
 	@echo "Creating user keypair: $(@F)"
-	@ssh-keygen -t ed25519 -f $@ -N "$(file < $<)" -C "$(@:../private/users/%.sshkey=%)@LocalEGA" &>/dev/null
+	@ssh-keygen -t ed25519 -f $@ -N "$(shell cat $<)" -C "$(@:../private/users/%.sshkey=%)@LocalEGA" &>/dev/null
 	@chmod 400 $@
 # Making the key readable, because when it's injected inside the container
 # it retains the permissions. Therefore, originally 400, will make it unreadable to the lega user.
 
 ../private/users/%.json: ../private/users/%.sshkey | ../private/users/serial
 	@echo "Creating user file: $(@F)"
-	@python -m run.cega.user $(@:../private/users/%.json=%) $(file < ../private/users/serial) '$(@:.json=.passphrase)' < $(<:=.pub) > $@
-	@$(file > ../private/users/serial,$(shell echo "$$(($(file < ../private/users/serial)+1))"))
+	@python -m run.cega.user $(@:../private/users/%.json=%) $(shell cat $|) '$(@:.json=.passphrase)' < $(<:=.pub) > $@
+	@echo -n "$$(($(shell cat $|)+1))" > $|
 
 ../private/users/%.passphrase: | ../private/users
 	@echo "Creating user passphrase: $(@F)"
