1a52eb35a372f1e5c1c9286b6b427091e10c83ef
Frédéric Haziza <frederic.haziza@crg.eu>
2020-05-21 02:37:34 +0200

    When only restarting, the entrypoint should be recreate the files

--- deploy/bootstrap/lega-entrypoint.sh
+++ deploy/bootstrap/lega-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
 
-cp /etc/ega/ssl.key /etc/ega/ssl.key.lega
-chmod 400 /etc/ega/ssl.key.lega
+if ! test -e /etc/ega/ssl.key.lega; then
+    cp /etc/ega/ssl.key /etc/ega/ssl.key.lega
+    chmod 400 /etc/ega/ssl.key.lega
+fi
 
 exec $@
